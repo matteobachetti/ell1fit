dd432a98f7b302f1b94211c0590a5bfc179d8045
Matteo Bachetti <matteo@matteobachetti.it>
2025-02-15 18:58:49 +0100

    Remove test runner

--- ell1fit/_astropy_init.py
+++ ell1fit/_astropy_init.py
@@ -7,8 +7,3 @@ try:
     from .version import version as __version__
 except ImportError:
     __version__ = ""
-
-# Create the test function for self test
-from astropy.tests.runner import TestRunner
-
-test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
